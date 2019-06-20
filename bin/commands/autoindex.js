cf0fbf96e553d612369ad7231e1533bfdbed93e2
Ken Snyder <ken@ken.net>
2019-06-19 18:11:13 -0700

    autoindex

--- bin/commands/autoindex.js
+++ bin/commands/autoindex.js
@@ -12,6 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const globby_1 = __importDefault(require("globby"));
+const fs_1 = __importDefault(require("fs"));
+const chalk_1 = __importDefault(require("chalk"));
 /**
  * Finds all `index.ts` and `index.js` files and looks for the `#autoindex`
  * signature. If found then it _auto_-builds this file based on files in
@@ -20,7 +22,27 @@ const globby_1 = __importDefault(require("globby"));
 function handler() {
     return __awaiter(this, void 0, void 0, function* () {
         const paths = yield globby_1.default(["**/index.ts", "**/index.js", "!node_modules"]);
-        console.log(paths);
+        const filesToComplete = yield findAutoCompleteFiles(paths);
     });
 }
 exports.handler = handler;
+function findAutoCompleteFiles(paths) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const result = [];
+        for (const path of paths) {
+            const fileString = fs_1.default.readFileSync(path, { encoding: "utf-8" });
+            if (fileString.includes("// #autoindex:")) {
+                result.push(path);
+            }
+        }
+        if (result.length === 0) {
+            console.log(`- Scanned through ${chalk_1.default.bold(String(paths.length))} ${chalk_1.default.italic("index")} files but none of them were "autoindex" files.\n`);
+            console.log(`${chalk_1.default.bold("  Note: ")}${chalk_1.default.dim.italic('to make an "index.ts" or "index.js" file an "autoindex file"')}`);
+            console.log(chalk_1.default.dim.italic("  you must add in the following to your index file (ideally on the first line):\n"));
+            console.log("  " + chalk_1.default.whiteBright.bgBlue("//#autoindex:[CMD] \n"));
+            console.log(chalk_1.default.dim.italic("  where the valid commands are (aka, CMD from above): ") +
+                chalk_1.default.italic("named,defaults"));
+        }
+        console.log();
+    });
+}
--- src/commands/autoindex.ts
+++ src/commands/autoindex.ts
@@ -1,4 +1,6 @@
 import globby from "globby";
+import fs from "fs";
+import chalk from "chalk";
 /**
  * Finds all `index.ts` and `index.js` files and looks for the `#autoindex`
  * signature. If found then it _auto_-builds this file based on files in
@@ -6,5 +8,39 @@ import globby from "globby";
  */
 export async function handler() {
   const paths = await globby(["**/index.ts", "**/index.js", "!node_modules"]);
-  console.log(paths);
+  const filesToComplete = await findAutoCompleteFiles(paths);
+}
+
+async function findAutoCompleteFiles(paths: string[]) {
+  const result: string[] = [];
+  for (const path of paths) {
+    const fileString = fs.readFileSync(path, { encoding: "utf-8" });
+    if (fileString.includes("// #autoindex:")) {
+      result.push(path);
+    }
+  }
+  if (result.length === 0) {
+    console.log(
+      `- Scanned through ${chalk.bold(String(paths.length))} ${chalk.italic(
+        "index"
+      )} files but none of them were "autoindex" files.\n`
+    );
+    console.log(
+      `${chalk.bold("  Note: ")}${chalk.dim.italic(
+        'to make an "index.ts" or "index.js" file an "autoindex file"'
+      )}`
+    );
+    console.log(
+      chalk.dim.italic(
+        "  you must add in the following to your index file (ideally on the first line):\n"
+      )
+    );
+
+    console.log("  " + chalk.whiteBright.bgBlue("//#autoindex:[CMD] \n"));
+    console.log(
+      chalk.dim.italic("  where the valid commands are (aka, CMD from above): ") +
+        chalk.italic("named,defaults")
+    );
+  }
+  console.log();
 }
