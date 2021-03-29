5db1a77d44a850ed3334597f80be9e751ffe24d6
Ken Snyder <ken@ken.net>
2021-03-29 11:42:41 -0700

    chore: updated deps chore: starting to add base64 feature to ssm cli

--- src/commands/ssm/public/examples.ts
+++ src/commands/ssm/public/examples.ts
@@ -1,1 +1,6 @@
-export const examples = ["do ssm list", "do ssm list --profile myproject", "do ssm add DEV/TRELLO/SID abcdefg1234"];
+export const examples = [
+  "do ssm list",
+  "do ssm list --profile myproject",
+  "do ssm add DEV/TRELLO/SID abcdefg1234",
+  "do ssm add DEV/FIREBASE --base64 adfasdfadsf",
+];
