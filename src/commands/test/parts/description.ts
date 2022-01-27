7081b40a57ef0beea3abe8c67fbb47602415dda9
Ken Snyder <ken@ken.net>
2022-01-27 14:23:24 -0800

    removed build and test commands as these should just be proxied to scripts

--- src/commands/test/parts/description.ts
+++
@@ -1,3 +0,0 @@
-export function description() {
-  return "Test some or all of your tests and incorporate useful test data without effort.";
-}
--- src/commands/test/private/other.ts
+++
@@ -1,9 +0,0 @@
-import chalk from "chalk";
-
-const otherFramework = async (_args: string[]) => {
-  console.log(
-    chalk`- Your default framework for unit testing is set to {bold other} which do-devops doesn't know how to work with.`
-  );
-};
-
-export default otherFramework;
